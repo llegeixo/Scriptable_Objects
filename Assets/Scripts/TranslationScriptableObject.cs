6e05bbc22ba00a50c202580f88404921bfa079ed
llegeixo <gabriel.tallon.lopez@gmail.com>
2024-04-09 20:40:27 +0200

    ejercicio canva commit

---
+++ Assets/Scripts/TranslationScriptableObject.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Translation", menuName = "Translation")]
+public class TranslationScriptableObject : MonoBehaviour
+{
+    public string languageName;
+    public string translatedText;
+
+}
+
